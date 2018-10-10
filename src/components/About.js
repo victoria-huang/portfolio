8ec25e10b391ca4a4fe6f9a1b86c075f51b5b9ff
victoria-huang <vctrah@gmail.com>
2018-10-10 16:34:25 -0400

    add hamburger menu

--- src/components/About.js
+++ src/components/About.js
@@ -7,6 +7,9 @@ const About = (props) => {
       <div className='center'>
         <div className='row-container'>
           <div className='column-left'>
+            <img src='assets/profile.jpg' alt='profile' />
+          </div>
+          <div className='column-right'>
             <h1>Hi, I'm Victoria!</h1>
             <p>
               I am a full-stack web developer and pharmacist based in New York City.
@@ -14,14 +17,15 @@ const About = (props) => {
               projects.
             </p>
             <hr className='black-hr' />
+            <h3>Technical Skills</h3>
+            <hr className='black-hr' />
+
             <p>
               React, React Native, Redux, Node, Express, JavaScript, Ruby, Rails, Java, RSpec, SQL,
               PostgreSQL, HTML, CSS, Canvas, Bootstrap, Semantic, Git, Heroku
             </p>
           </div>
-          <div className='column-right'>
-            <img src='assets/profile.jpg' height='400vh' width='300vw' alt='profile' />
-          </div>
+
         </div>
       </div>
     </ParallaxLayer>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,13 +2,20 @@ import React, { Fragment } from 'react'
 
 const Nav = (props) => {
   return (
-    <Fragment>
-      <a onClick={ () => props.scroll(0) }>Hello</a>
-      &nbsp; | &nbsp;
-      <a onClick={ () => props.scroll(1) }>About</a>
-      &nbsp; | &nbsp;
-      <a onClick={ () => props.scroll(2) }>Portfolio</a>
-    </Fragment>
+    <nav role="navigation">
+      <div id="menuToggle">
+        <input type="checkbox" />
+          <span></span>
+          <span></span>
+          <span></span>
+
+        <ul id="menu">
+          <a onClick={ () => props.scroll(0) }><li>Hello</li></a>
+          <a onClick={ () => props.scroll(1) }><li>About</li></a>
+          <a onClick={ () => props.scroll(2) }><li>Portfolio</li></a>
+        </ul>
+      </div>
+    </nav>
   )
 }
 
