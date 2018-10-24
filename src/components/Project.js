c9f53e3ec115331517b73ec80e96a9f0858cdd70
victoria-huang <vctrah@gmail.com>
2018-10-24 17:02:58 -0400

    change project back to functional comp

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,37 +1,35 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { ParallaxLayer } from 'react-spring'
 import { Link } from 'react-router-dom'
 import ProjectDetail from './ProjectDetail'
 
-class Project extends Component {
-  render() {
-    return (
-      <ParallaxLayer offset={ this.props.offset } speed={-0.2} style={{ backgroundColor: '#006E6D' }}>
-        <div className='container-item'>
-          <div>
-            <span className='overlay-text'>
-              <h1 className='large'>{ this.props.name }</h1>
-            </span>
-            <img src={ this.props.image ?
-              this.props.image
-              :
-              'https://images.pexels.com/photos/754082/pexels-photo-754082.jpeg?auto=compress&cs=tinysrgb&h=350'
-            } height='350px' width='300px' alt={ `${this.props.name} preview` } />
-          </div>
-          <ProjectDetail />
-        </div>
-
-        {
-          this.props.last &&
-          <span className='bottom-button'>
-            <Link to='/contact'>
-              <button className='button-2'>Contact me.</button>
-            </Link>
+const Project = (props) => {
+  return (
+    <ParallaxLayer offset={ props.offset } speed={-0.2} style={{ backgroundColor: '#006E6D' }}>
+      <div className='container-item'>
+        <div>
+          <span className='overlay-text'>
+            <h1 className='large'>{ props.name }</h1>
           </span>
-        }
-      </ParallaxLayer>
-    )
-  }
+          <img src={ props.image ?
+            props.image
+            :
+            'https://images.pexels.com/photos/754082/pexels-photo-754082.jpeg?auto=compress&cs=tinysrgb&h=350'
+          } height='350px' width='300px' alt={ `${props.name} preview` } />
+        </div>
+        <ProjectDetail />
+      </div>
+
+      {
+        props.last &&
+        <span className='bottom-button'>
+          <Link to='/contact'>
+            <button className='button-2'>Contact me.</button>
+          </Link>
+        </span>
+      }
+    </ParallaxLayer>
+  )
 }
 
 export default Project
