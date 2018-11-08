f24bfe1dc1f36ef6ed5fcac051ab6a160daa7c8c
victoria-huang <vctrah@gmail.com>
2018-11-07 22:29:18 -0500

    edit languages

--- src/components/About.js
+++ src/components/About.js
@@ -16,8 +16,10 @@ const About = (props) => {
             <hr className='white-hr' />
 
             <p>
-              React, React Native, Redux, Node, Express, JavaScript, Ruby, Rails, Java, RSpec, SQL,
-              PostgreSQL, HTML, CSS, Canvas, Bootstrap, Semantic, Git, Heroku
+              // React, React Native, Redux, Node, Express, JavaScript, Ruby, Rails, Java, RSpec, SQL,
+              // PostgreSQL, HTML, CSS, Canvas, Bootstrap, Semantic, Git, Heroku
+              JavaScript, React, React Native, Redux, Node.js, Express, Ruby, Rails, Java, SQL,
+              PostgreSQL, HTML, CSS, Git
             </p>
           </div>
           <div className='column-right'>
